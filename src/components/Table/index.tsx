8be9584b6430dd886fd5ef904bacbe37ed237e64
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-06-08 03:43:14 +0800

    feat: make date string shorter for better layout.

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -22,11 +22,30 @@ type Props = {
   search?: string
 }
 
+function tsToStr(ts: number) {
+  const date = new Date(ts * 1000);
+  const year = String(date.getFullYear()).padStart(4, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return formattedDate;
+}
+
+
+
 export default function Table({ items: srcItems, search }: Props) {
   let items = srcItems.sort((a, b) => (a.name > b.name) ? 1 : -1);
   if (search) {
     items = items.filter(u => u.name.toLowerCase().indexOf(search.toLowerCase()) != -1)
   }
+
+
+  const timezone = new Date().getTimezoneOffset() / -60;
+
   return (
     <table className={styles['container']}>
       <thead>
@@ -35,7 +54,7 @@ export default function Table({ items: srcItems, search }: Props) {
             镜像名称
           </th>
           <th className={styles['last-update']}>
-            上次更新
+            上次更新 (UTC{timezone > 0 && '+'}{timezone})
           </th>
           <th className={styles['help']}>
             帮助
@@ -47,7 +66,7 @@ export default function Table({ items: srcItems, search }: Props) {
         {items.map(u => (
           <tr key={u.name}>
             <th className={styles['name']}>{u.name}</th>
-            <th className={styles['last-update']}>{u.last_update}</th>
+            <th className={styles['last-update']}>{tsToStr(u.last_update_ts)}</th>
             <th className={styles['help']}></th>
           </tr>
         ))}
