45b1061b1d88646d361ed95d689d0116c1915b71
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-06-09 07:58:05 +0800

    fix: fix blog link error in "edit this page".

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://gitee.com/dzm91_hust/hust-mirrors/',
+            'https://gitee.com/dzm91_hust/hust-mirrors/blob/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
