1e7da996187f5b0e62bbc47adecf76a174f8a7ed
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-06-09 07:50:22 +0800

    feat: implement new feature of two input components.
    Following features are implemented.
    * adaptive color of select
    * suppport label top layout

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react'
 import Select from 'react-select'
 import styles from './index.module.css'
@@ -18,8 +19,10 @@ type Props = {
 export default ({ label, value, onChange, items, labelTop }: Props) => {
   return (
     <div>
-      <span className={styles.label}>{label}</span>
-      <Select<SelectItem>
+      <span className={clsx(styles.label, labelTop && styles['label-top'])}>{label}</span>
+      <
+        // @ts-ignore
+        Select<SelectItem>
         value={items.find(u => u.value == value)}
         classNames={{
           container: () => labelTop ? "" : styles.container,
@@ -31,6 +34,20 @@ export default ({ label, value, onChange, items, labelTop }: Props) => {
           colors: {
             ...theme.colors,
             primary: 'var(--ifm-color-primary)',
+            primary75: 'var(--ifm-color-emphasis-300)',
+            primary50: 'var(--ifm-color-emphasis-300)',
+            primary25: 'var(--ifm-color-emphasis-200)',
+            neutral0: 'var(--ifm-color-emphasis-0)',
+            neutral5: 'var(--ifm-color-emphasis-500)',
+            neutral10: 'var(--ifm-color-emphasis-600)',
+            neutral20: 'var(--ifm-color-emphasis-600)',
+            neutral30: 'var(--ifm-color-emphasis-700)',
+            neutral40: 'var(--ifm-color-emphasis-700)',
+            neutral50: 'var(--ifm-color-emphasis-800)',
+            neutral60: 'var(--ifm-color-emphasis-800)',
+            neutral70: 'var(--ifm-color-emphasis-900)',
+            neutral80: 'var(--ifm-color-emphasis-900)',
+            neutral90: 'var(--ifm-color-emphasis-900)'
           }
         })}
       >
