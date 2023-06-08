dbca41b795c09e1ee7755fa3e565e36621a67bd6
Dongliang Mu <dzm91@hust.edu.cn>
2023-06-08 18:48:19 +0800

    mirrors.config.js: add more description of normal mirrors
    Signed-off-by: Dongliang Mu <dzm91@hust.edu.cn>

--- mirrors.config.js
+++ mirrors.config.js
@@ -12,7 +12,12 @@ const mirrors = [
   { id: 'linux.git', type: 'git', description: 'Linux 内核主线仓库', helpID: 'linux.git' },
   { id: 'linux-stable.git', type: 'git', description: 'Linux 内核稳定分支仓库', helpID: 'linux.git' },
   { id: 'linux-next.git', type: 'git', description: 'Linux 内核源开发分支仓库', helpID: 'linux-next.git' },
-  { id: 'qemu.git', type: 'git', description: 'QEMU 主线仓库', helpID: 'qemu.git' },
+  { id: 'archlinux', type: 'normal', description: 'Archlinux 软件仓库', helpID: 'archlinux' },
+  { id: 'debian', type: 'normal', description: 'Debian GNU/Linux 软件仓库', helpID: 'debian' },
+  { id: 'deepin', type: 'normal', description: 'Deepin 软件仓库', helpID: 'deepin' },
+  { id: 'openeuler', type: 'normal', description: 'OpenEuler 软件仓库', helpID: 'openeuler' },
+  { id: 'openkylin', type: 'normal', description: 'OpenKylin 软件仓库', helpID: 'openkylin' },
+  { id: 'ubuntu', type: 'normal', description: 'Ubuntu 软件仓库', helpID: 'ubuntu' },
 ]
 
 
