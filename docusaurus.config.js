hust-open-atom-club/hust-mirrors

commit 514079e99ce1f28c79698505944b676eeebe2468

conf: edit config file to support mirror meta.

docusaurus.config.js
@@ -4,17 +4,17 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const mirrorConfig = {
-  title: "华中科技大学开源镜像站",
-  desc: "欢迎来到华中科技大学开源镜像站，该站点由华中科技大学为你呈现。",
-  url: "https://hustmirror.cn"
-}
+const { config: mirrorConfig, mirrors } = require('./mirrors.config')
+
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: mirrorConfig.title,
   tagline: mirrorConfig.desc,
-  favicon: 'img/favicon.png',
+  favicon: 'img/favicon.ico',
+  customFields: {
+    mirrors
+  },
 
   // Set the production url of your site here
   url: mirrorConfig.url,
@@ -92,7 +92,7 @@ const config = {
       },
       footer: {
         style: 'light',
-        copyright: `HUSTMirror Team`,
+        copyright: `华中科技大学开源镜像站由 华中科技大学网络中心 提供支持。`
       },
       prism: {
         theme: lightCodeTheme,

mirrors.config.js
@@ -1,0 +1,26 @@
+
+/** @type{{
+ * id: string; 
+ * type?: 'normal' | 'git';
+ * description?: string;
+ * forceShown?: boolean;
+ * link?: string;
+ * helpID?: string;
+ * }}[] */
+const mirrors = [
+  { id: 'linux.git', type: 'git', description: 'Linux 内核源代码的Git仓库', helpID: 'linux.git' },
+  { id: 'linux-stable.git', type: 'git', description: 'Linux 内核源代码的Git仓库，稳定版分支', helpID: 'linux.git' },
+  { id: 'linux-next.git', type: 'git', description: 'Linux 内核源代码的Git仓库，开发分支', helpID: 'linux-next.git' },
+]
+
+
+const config = {
+  title: "华中科技大学开源镜像站",
+  desc: "欢迎来到华中科技大学开源镜像站，该站点由华中科技大学为你呈现。",
+  url: "https://hustmirror.cn"
+}
+
+module.exports = {
+  config,
+  mirrors
+};

commit 183edff5d610983fb74ef3df858cd79aee6dd219

fix: replace "favicon.ico" with "favicon.png"
Fixes: 514079e (conf: edit config file to support mirror meta.)

docusaurus.config.js
@@ -11,7 +11,7 @@ const { config: mirrorConfig, mirrors } = require('./mirrors.config')
 const config = {
   title: mirrorConfig.title,
   tagline: mirrorConfig.desc,
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   customFields: {
     mirrors
   },