18e45038d85c4020241b155a875a4d16e215978a
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-09-09 02:57:54 +0800

    feat: add color change in fancyindex.

--- src/pages/fancyindex/index.tsx
+++ src/pages/fancyindex/index.tsx
@@ -76,6 +76,32 @@ const clientScript = `
     }
   })
 })();
+
+/** add script for change theme button */
+(function() {
+  var defaultMode = 'light';
+  function setTheme(th) {
+    document.documentElement.setAttribute('data-theme', th);
+    theme = th;
+    try {
+      localStorage.setItem('theme', th);
+    } catch (err) {}
+  }
+  function getStoredTheme() {
+    var theme = null;
+    try {
+      theme = localStorage.getItem('theme');
+    } catch (err) {}
+    return theme;
+  }
+  var theme = getStoredTheme() || defaultMode;
+  var btn = document.querySelector(".navbar__items--right :nth-child(2) button")
+  btn.disabled = false;
+  btn.style.cursor = "pointer";
+  btn.addEventListener('click', () => {
+    setTheme(theme == 'dark' ? 'light' : 'dark')
+  })
+})()
 `;
 
 function FancyIndexInjectionContainer() {
