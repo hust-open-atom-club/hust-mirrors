84472f001e83e458ca9ea6cd5df4d3cea532525e
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-09-09 02:15:50 +0800

    fix: fix fancyindex bugs.

--- plugin/fancyindex.js
+++ plugin/fancyindex.js
@@ -12,11 +12,18 @@ const fancyindexPlugin = {
     const tag = "<!-- fancyindex -->";
     const content = await fs.readFile(inputFile, 'utf-8');
     const files = content.split(tag);
-    if(files.length != 2) console.error("fancyindex: multiple or no tag found");
-    await fs.writeFile(outputHeader, files[0]);
-    await fs.writeFile(outputFooter, files[1]);
+    if (files.length != 2) console.error("fancyindex: multiple or no tag found");
+    let header = files[0];
+    // remove language select
+    header = header.replace(/<div class="navbar__item dropdown dropdown--hoverable dropdown--right">.+?<\/div>/, "");
+    header = header.replace(/<link rel="preload".+?>/g, "");
+    await fs.writeFile(outputHeader, header);
+    // remove script
+    let footer = files[1];
+    footer = footer.replace(/<script src=".+"><\/script>/g, '');
+    await fs.writeFile(outputFooter, footer);
     await fs.rm(inputFile);
-  }
+  },
 }
 
 module.exports = () => fancyindexPlugin;
--- src/pages/fancyindex/index.tsx
+++ src/pages/fancyindex/index.tsx
@@ -1,49 +1,88 @@
-import { translate } from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from './index.module.css'
-import { useLocation } from "@docusaurus/router";
-import { useDocMetas, useMirrorMetas } from '@site/src/utils/mirrorUtils';
-import CodeBlock from '@theme/CodeBlock'
-import BrowserOnly from "@docusaurus/BrowserOnly";
+import { useMirrorMetas } from "@site/src/utils/mirrorUtils";
 
 function Breadcrumb() {
-  const { pathname } = useLocation();
-  const items = pathname.split("/").map((curr, index, list) => {
-    const prev = '/' + list.slice(0, index).join("/");
-    return {
-      label: curr || '/',
-      href: prev + curr
-    }
-  });
   return <nav className={clsx("theme-doc-breadcrumbs", styles.breadcrumbs)} aria-label="Breadcrumbs">
-    <ul className="breadcrumbs">
-      {
-        items.map((item, index) => <li className="breadcrumbs__item" key={item.href}>
-          {
-            index !== items.length - 1 ?
-              <a aria-label={`Link to ${item.label}`} className="breadcrumbs__link" href={item.href}>{item.label}</a>
-              :
-              <span className="breadcrumbs__link">{item.label}</span>
-          }
-          <meta itemProp="position" content={index.toString()} />
-        </li>
-        )
-      }
+    <ul className="breadcrumbs" id='fancyindex__breadcrumbs'>
     </ul>
   </nav>
-
 }
+
+const mirrorMeta = JSON.stringify(useMirrorMetas());
+
+const clientScript = `
+/** Render BreadCrumbs */
+(function () {
+  var pathname = window.location.pathname;
+  var items = pathname.split("/").map((curr, index, list) => {
+    if (!curr) return undefined;
+    var prev = list.slice(0, index).join("/");
+    return {
+      label: curr,
+      href: prev + '/' + curr
+    }
+  }).filter(u => !!u);
+  if (items.length < 2) return;
+  var breadcrumbsContainr = document.getElementById('fancyindex__breadcrumbs');
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var li = document.createElement('li');
+    li.className = 'breadcrumbs__item';
+    var ele;
+    if (i !== items.length - 1) {
+      ele = document.createElement('a');
+      ele.href = item.href;
+    }
+    else ele = document.createElement('span');
+
+    ele.ariaLabel = "Link to " + item.label;
+    ele.className = "breadcrumbs__link";
+    // cautious about XSS
+    ele.innerText = item.label;
+    li.appendChild(ele)
+    breadcrumbsContainr.appendChild(li);
+  }
+})();
+
+/** Render Header */
+(function () {
+  var pathname = window.location.pathname;
+  var mirrorid = pathname.split("/")[1];
+  var metas = ${ mirrorMeta } /** Inject Meta Here */
+  var meta = metas.find(u => u.id == mirrorid);
+  var displayName = meta ? (meta.displayName || meta.id) : mirrorid;
+
+  var titleDoms = document.getElementsByClassName('fancyindex__mirrorname');
+  var lastUpdate = document.getElementById('fancyindex__lastUpdate');
+  var helpBtn = document.getElementById('fancyindex__helpbutton');
+
+  for (var i = 0; i < titleDoms.length; i++) {
+    var title = titleDoms[i];
+    title.innerText = mirrorid;
+  }
+  if (meta) helpBtn.href = "/docs/" + meta.helpID || meta.id;
+})();
+
+/** set date string */
+(function () {
+  document.querySelectorAll("#list tbody tr td:nth-child(3)").forEach((e) => {
+    var s = new Date(e.innerText);
+    if (!isNaN(s.getTime())) {
+      var u = ("000" + s.getFullYear()).substr(-4) + "-" + ("0" + (s.getMonth() + 1)).substr(-2) + "-" + ("0" + s.getDate()).substr(-2) + " " + ("0" + s.getHours()).substr(-2) + ":" + ("0" + s.getMinutes()).substr(-2);
+      e.innerText = u;
+    }
+  })
+})();
+`;
 
 function FancyIndexInjectionContainer() {
   return <div className={styles.table} dangerouslySetInnerHTML={
     {
-      __html: `<h1 style="display:none">
-<!-- fancyindex -->
-<script>
-// modify injection dom
-</script>`}
+      __html: `<h1 style="display:none"/><!-- fancyindex -->`
+    }
   }>
     {/* fancy index content */}
   </div>
@@ -51,35 +90,23 @@ function FancyIndexInjectionContainer() {
 }
 
 function Header() {
-  const { pathname } = useLocation();
-  const mirrorid = pathname.split("/")[1];
-  const metas = useMirrorMetas();
-  const meta = metas.find(u => u.id == mirrorid);
-  const displayName = meta ? (meta.displayName || meta.id) : mirrorid;
-
   return <header className={clsx('hero hero--primary', styles.header)}>
     <div className={styles['title-container']}>
-      <BrowserOnly>
-        {() => <>
-          <div className={styles.title}>
-            <h1>
-              {translate({
-                id: 'mirror.release',
-                message: `${displayName} 的文件索引`
-              })}
-            </h1>
-            <p>上次同步时间：N/A</p>
-          </div>
-          <div className={styles.blist}>
-            <button className="button button--secondary">
-              查看帮助
-            </button>
-            <div className={styles.cliBlock}>
-              hustmirror deploy {mirrorid}
-            </div>
-          </div>
-        </>}
-      </BrowserOnly>
+      <div className={styles.title}>
+        <h1>
+          <span className='fancyindex__mirrorname'></span>
+          <span>的文件索引</span>
+        </h1>
+        <p id='fancyindex__lastUpdate'>上次同步时间：N/A</p>
+      </div>
+      <div className={styles.blist}>
+        <a className="button button--secondary" id="fancyindex__helpbutton">
+          查看帮助
+        </a>
+        <div className={styles.cliBlock}>
+          hustmirror deploy <span className="fancyindex__mirrorname"></span>
+        </div>
+      </div>
     </div>
   </header >
 }
@@ -92,6 +119,9 @@ export default function FileIndexPage() {
       <Breadcrumb />
       <FancyIndexInjectionContainer />
     </div>
+    <script dangerouslySetInnerHTML={{
+      __html: clientScript
+    }}></script>
   </Layout>
 
 }
