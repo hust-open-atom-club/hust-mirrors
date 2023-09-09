60ddd00d64a4ad61aa657d3b83f9b3eab6d337d0
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-09-09 14:36:35 +0800

    chore: write translations of footer.

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useThemeConfig } from '@docusaurus/theme-common';
 import clsx from 'clsx';
 import styles from './index.module.css'
+import Translate from '@docusaurus/Translate';
 
 function Footer() {
   const { footer } = useThemeConfig();
@@ -17,10 +18,24 @@ function Footer() {
       <div className={clsx('container', ' container-fluid', styles.container)}>
         {(logo || copyright) && (
           <div className="footer__bottom" style={{ maxWidth: 650 }}>
-            <div>华中科技大学开源镜像站由 <a href='https://ncc.hust.edu.cn'>华中科技大学网络中心</a> 提供支持。</div>
-            <div>该镜像站收录了主流开源操作系统的软件源、安装镜像，包管理器的仓库，以及常用开源软件源代码，
-              是 deepin, openEuler, openKylin 等多个国产操作系统的官方源。</div>
-            <div>该镜像站目前是华中地区规模最大，速度最快，收录最全的开源软件镜像站。除此以外，提供了发行版ISO下载、完整的使用帮助文档和CLI工具的支持。</div>
+            <div>
+              <Translate id='mirror.footer.desc0_0'> 华中科技大学开源镜像站由</Translate>
+              <a href='https://ncc.hust.edu.cn'>
+                <Translate id='mirror.footer.desc0_1'>华中科技大学网络中心</Translate>
+              </a>
+              <Translate id='mirror.footer.desc0_2'>提供支持。</Translate>
+            </div>
+            <div>
+              <Translate id='mirror.footer.desc1'>
+                该镜像站收录了主流开源操作系统的软件源、安装镜像，包管理器的仓库，以及常用开源软件源代码，
+                是 deepin, openEuler, openKylin 等多个国产操作系统的官方源。
+              </Translate>
+            </div>
+            <div>
+              <Translate id='mirror.footer.desc2'>
+                该镜像站目前是华中地区规模最大，速度最快，收录最全的开源软件镜像站。除此以外，提供了发行版ISO下载、完整的使用帮助文档和CLI工具的支持。
+              </Translate>
+            </div>
           </div>
         )}
         <div className={styles.logos}>
