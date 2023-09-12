6ae9bcac37d2ac6bc73037fca3bf0d8070e2eb9e
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-09-12 21:13:29 +0800

    feat: add sidebar icons.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -5,6 +5,12 @@ import Translate from '@docusaurus/Translate'
 import { DomainMeta } from '@site/meta.config'
 import { useDomainMetas } from '@site/src/utils/mirrorUtils'
 
+import DomainIcon from '@site/static/icons/domain.svg'
+import ContactIcon from '@site/static/icons/contact.svg'
+import HelpIcon from '@site/static/icons/help.svg'
+import ImageIcon from '@site/static/icons/image.svg'
+import LinkIcon from '@site/static/icons/link.svg'
+
 interface Props extends React.HTMLProps<HTMLDivElement> { }
 
 
@@ -22,10 +28,10 @@ export default function SideBar(props: Props) {
   return (
     <div {...props}>
       <div className={styles.side}>
-        <a><h3><Translate id='mirror.sidebar.chooseDomain'>域名选择</Translate></h3></a>
+        <a><h3><DomainIcon /><Translate id='mirror.sidebar.chooseDomain'>域名选择</Translate></h3></a>
         <Domains domains={domains}></Domains>
 
-        <a><h3><Translate id='mirror.sidebar.release'>发行版下载</Translate></h3></a>
+        <a><h3><ImageIcon /><Translate id='mirror.sidebar.release'>发行版下载</Translate></h3></a>
         <p>
           <Translate id='mirror.sidebar.releaseDesc'>
             这里为您提供各大主流 Linux 发行版的安装镜像 / ISO 文件，
@@ -41,7 +47,7 @@ export default function SideBar(props: Props) {
         </Link>
 
 
-        <a><h3><Translate id='mirror.sidebar.help'>镜像站帮助</Translate></h3></a>
+        <a><h3><HelpIcon /><Translate id='mirror.sidebar.help'>镜像站帮助</Translate></h3></a>
         <p>
           <Translate id='mirror.sidebar.helpDesc'>
             如果你不了解如何配置替换软件镜像源，
@@ -61,7 +67,7 @@ export default function SideBar(props: Props) {
           </Translate>
         </p>
 
-        <a><h3><Translate id="mirror.sidebar.contactus">联系我们</Translate></h3></a>
+        <a><h3><ContactIcon /><Translate id="mirror.sidebar.contactus">联系我们</Translate></h3></a>
         <p>
           <a href="https://gitee.com/dzm91_hust/hust-mirrors/issues"><Translate id='mirror.sidebar.submit'>提交问题</Translate></a>
         </p>
@@ -69,7 +75,7 @@ export default function SideBar(props: Props) {
           <a href='mailto:mirror_support@hust.edu.cn'><Translate id='mirror.sidebar.sendemail'>发送邮件</Translate></a>
         </p>
 
-        <a><h3><Translate id="mirror.sidebar.links">友情链接</Translate></h3></a>
+        <a><h3><LinkIcon /><Translate id="mirror.sidebar.links">友情链接</Translate></h3></a>
         <p>
           <Link to='/syncing-status'><Translate id="mirror.sidebar.status">系统状态</Translate></Link>
         </p>
