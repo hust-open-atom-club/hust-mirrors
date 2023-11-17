c4c3b938209b47e541e57491e44def9f03e24a2d
Dongliang Mu <dzm91@hust.edu.cn>
2023-11-17 16:03:19 +0800

    revise welcome statement

--- mirrors.config.js
+++ mirrors.config.js
@@ -2,7 +2,7 @@ const config = {
   title: "华中科技大学开源镜像站 | HUST Mirror",
   mainTitle: "华中科技大学开源镜像站",
   url: "https://mirrors.hust.college",
-  welcome: "欢迎来到华中科技大学开源镜像站，该站点由华中科技大学为你呈现。"
+  welcome: "欢迎来到华中科技大学开源镜像站。"
 }
 
 module.exports = config;
